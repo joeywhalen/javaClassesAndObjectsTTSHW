4646888d8f3dde40aa5f26138948a39aa925bfe2
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 16:39:39 -0500

    updated Pet to include Getters and Setters

--- src/javaClassesAndObjectsTTSHW/Pet.java
+++ src/javaClassesAndObjectsTTSHW/Pet.java
@@ -2,23 +2,51 @@ package javaClassesAndObjectsTTSHW;
 
 public class Pet {
 
-	public static void main(String[] args) {
 		// TODO create a Pet Class with 4 instance variables, 2 constructors, 3 Getters and 3 Setters
-		private String petName;
+		private String name;
 		private Integer age;
 		private String location;
 		private String type;
 		
-		public Pet() {}
-		
-		public Pet(String petName, int age, String location, String type) {
-			this.petName = petName;
+		public Pet(String name, int age, String type){
+			this.name = name;
 			this.age = age;
-			this.location = location;
 			this.type = type;
 		}
 		
+		public Pet(String name, int age, String location) {
+			this.name = name;
+			this.age = age;
+			this.location = location;
+		}
 		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public void setName(String name)
+		{
+			this.name= name;
+		}
+		
+		public int getAge()
+		{
+			return age;
+		}
+		
+		public void setAge(int age)
+		{
+			this.age = age;
+		}
+		
+		public String getType()
+		{
+			return type;
+		}
+		
+		public void setLocation(String location)
+		{
+			this.location = location;
+		}
 	}
-
-}
