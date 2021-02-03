b5c308fff1fde75e95aed33f33951f1e57bd680f
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 16:01:33 -0500

    update Pet to include instance variables

--- src/javaClassesAndObjectsTTSHW/Pet.java
+++ src/javaClassesAndObjectsTTSHW/Pet.java
@@ -4,7 +4,14 @@ public class Pet {
 
 	public static void main(String[] args) {
 		// TODO create a Pet Class with 4 instance variables, 2 constructors, 3 Getters and 3 Setters
-
+		private String petName;
+		private Integer age;
+		private String location;
+		private String type;
+		
+		public Pet() {}
+		
+		
 	}
 
 }
